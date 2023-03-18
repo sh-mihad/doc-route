4fb24632500506dce50947cf6aee8c8c71a5b838
sh-mihad <sh.mihad.cse@gmail.com>
2023-03-19 00:04:49 +0600

    some changes

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,15 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBsihUz3OBIDBDnBMXqAset57wuB_QflqI",
-  authDomain: "doc-route-811dc.firebaseapp.com",
-  projectId: "doc-route-811dc",
-  storageBucket: "doc-route-811dc.appspot.com",
-  messagingSenderId: "968228843539",
-  appId: "1:968228843539:web:f9df88396b1ea7821b4fe6"
+  apiKey: "AIzaSyCAWm2HqwfYx-VZ8Kg1C4x8855xofMdmBY",
+  authDomain: "docroute-9bc4c.firebaseapp.com",
+  projectId: "docroute-9bc4c",
+  storageBucket: "docroute-9bc4c.appspot.com",
+  messagingSenderId: "938212478503",
+  appId: "1:938212478503:web:a63d630420339a6f510802"
 };
 
 // Initialize Firebase
- const app = initializeApp(firebaseConfig);
-
- export default app;
+const app = initializeApp(firebaseConfig);
+export default app
