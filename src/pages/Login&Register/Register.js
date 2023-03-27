f56a789b9b8ef1a88c18793f33b4dfd787de8b9c
sh-mihad <sh.mihad.cse@gmail.com>
2023-03-28 04:09:54 +0600

    reg done

--- src/pages/Login&Register/Register.js
+++ src/pages/Login&Register/Register.js
@@ -17,47 +17,70 @@ const Register = () => {
         const name = data.name
         const email = data.email
         const password = data.password
-
+        const mobile = data.mobile;
+        const address = data.address;
+        const age = data.age;
+        const gender = data.gender
 
       
 
-        const formData = new FormData();
-        console.log(formData);
-        formData.append('image', data.photo[0]);
+        // const formData = new FormData();
+        // console.log(formData); 
+        // formData.append('image', data.photo[0]);
 
-        console.log(formData);
+        // console.log(formData);
 
-        fetch('https://api.imgbb.com/1/upload?key=2fbe1796a4bf3cd52ba5028ba7992a29', {
-            method: 'POST',
-            body: formData
-        })
-            .then(response => response.json())
-            .then(result => {
-                if (result.success) {
-                    const image = result.data.display_url
+        // fetch('https://api.imgbb.com/1/upload?key=2fbe1796a4bf3cd52ba5028ba7992a29', {
+        //     method: 'POST',
+        //     body: formData
+        // })
+        //     .then(response => response.json())
+        //     .then(result => {
+        //         if (result.success) {
+        //             const image = result.data.display_url
                     
-                    createUserAccoutn(email, password)
-                        .then(res => {
-                            const user = res.user
-                            console.log(user);
-                            updateName(name, image)
-                                .then(() => {
-                                    const currentUser = {
-                                        name,
-                                        email,
+        //             createUserAccoutn(email, password)
+        //                 .then(res => {
+        //                     const user = res.user
+        //                     console.log(user);
+        //                     updateName(name, image)
+        //                         .then(() => {
+        //                             const currentUser = {
+        //                                 name,
+        //                                 email,
 
-                                    }
+        //                             }
                                     
-                                    reset()
+        //                             reset()
 
-                                })
-                                .catch(err => console.log(err.message))
-                        })
-                        .catch(err => {
-                            console.log(err.message)
-                        })
-                }
-            })
+        //                         })
+        //                         .catch(err => console.log(err.message))
+        //                 })
+        //                 .catch(err => {
+        //                     console.log(err.message)
+        //                 })
+        //         }
+        //     })
+
+        const petaintData = {
+            name,
+            email,
+            password,
+            mobile,
+            address,
+            age,
+            gender,
+            
+           }
+    
+          fetch("http://localhost:5000/pateint",{
+            method: "POST",
+            headers:{
+                "content-type":"application/json"
+            },
+            body : JSON.stringify(petaintData)
+          }).then(res=>res.json())
+          .then(data=>console.log(data))
 
 
     }
@@ -117,6 +140,51 @@ const Register = () => {
                             {errors.password && <p className='text-sm mt-2 text-red-700'>{errors.password.message}</p>}
 
                         </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Mobile No</span>
+                            </label>
+                            <input type="text" placeholder="Mobile No" className="input input-bordered"
+                                {...register("mobile", {
+                                    required: "Invalid Name Fild",
+
+                                })}
+                            />
+                            {errors.mobile && <p className='text-sm mt-2 text-red-700'>{errors.mobile.message}</p>}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Address</span>
+                            </label>
+                            <input type="text" placeholder="Full Address" className="input input-bordered"
+                                {...register("address", {
+                                    required: "Invalid Name Fild",
+
+                                })}
+                            />
+                            {errors.address && <p className='text-sm mt-2 text-red-700'>{errors.address.message}</p>}
+                        </div>
+                        
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Age</span>
+                            </label>
+                            <input type="text" placeholder="Age" className="input input-bordered"
+                                {...register("age", {
+                                    required: "Invalid Name Fild",
+
+                                })}
+                            />
+                            {errors.age && <p className='text-sm mt-2 text-red-700'>{errors.age.message}</p>}
+                        </div>
+
+                       
+                        <select className="select input-bordered input w-full max-w-xs" {...register("gender")}>
+                            <option value="" hidden>Select Gender </option>
+                            <option value="Male">Male </option>
+                            <option value="Female">Female</option>
+                            <option value="Others">Others</option>
+                        </select>
 
                         <div className="form-control">
                             <label className="label">
@@ -127,6 +195,8 @@ const Register = () => {
                             })} />
 
                         </div>
+
+                        
 
 
                         <div className="form-control mt-6">
